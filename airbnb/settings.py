e7333b00cefaa502a969465ba3182540289fe147
KapitanKent <144911516+KapitanKent@users.noreply.github.com>
2025-03-22 23:49:23 +0800

    frontEnd

--- airbnb/settings.py
+++ airbnb/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbairbnb',
-        'USER': 'john earl',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'Kda00510',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
